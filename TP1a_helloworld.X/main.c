2c7b54b2c78e529a211031c32dba96795c700ab4
hugodef1 <158208394+hugodef1@users.noreply.github.com>
2026-01-13 15:20:50 +0100

    Implement delay function and modify LED control logic
    Added approximate delay function and updated main loop to alternate LED states.

--- TP1a_helloworld.X/main.c
+++ TP1a_helloworld.X/main.c
@@ -1,21 +1,27 @@
-#include "configbits.h" // Bits de configuration
-#include <xc.h>         // Definition des registres specifiques au uC
-
-
-#define _XTAL_FREQ 8000000
-
+#include <xc.h>
+#include "configbits.h"
+//Avec 60k cycles nous etions à 13 secondes au bout de 10 cycles
+//Nous observons qu'avec 48k cycles nous obtenons quasiment parfaitement 10 cycles en 10 secondes
+void delai_approx(void){
+    unsigned long i;
+    for (i=0; i<48000; i++) {
+    }
+}
 void main(void) {
-    /* Code d'initialisation */
-    TRISD = 0x00;
-    ANSELD = 0x00;
-    LATD = 0x00;
+    ANSELD = 0x00;  // TOUTES les broches du PORTD deviennent numériques
+    ANSELB = 0x00;  // TOUTES les broches du PORTB deviennent numériques
+    TRISD = 0x00;   //Mise à 0 des broches en sortie
+    TRISB = 0x00;   //Mise à 0 des broches en sortie
+    LATD = 0x00;    //Mise à 0 des bropches en sortie
+    LATB = 0x00;    //Mise à 0 des bropches en sortie
 
-    while(1){
-        /* Code a executer dans une boucle infinie */
-        
-        LATD = 0x0F;
-        __delay_ms(500);
-        LATD = 0xF0;
-        __delay_ms(500);
+    while(1) {
+        LATD = 0x0F; // D1-D4 allumées
+        LATB = 0x00; // D5-D8 éteintes
+        delai_approx();
+
+        LATD = 0x00; // D1-D4 éteintes
+        LATB = 0x0F; // D5-D8 allumées
+        delai_approx();
     }
 }
